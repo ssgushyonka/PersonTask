1264853e49dd083534a992a42705ca29da64c1d9
ssgushyonka <orangeee04@gmail.com>
2025-02-19 21:40:10 +0300

    Initial Commit

---
+++ PersonTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonTask
+//
+//  Created by Элина Борисова on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
